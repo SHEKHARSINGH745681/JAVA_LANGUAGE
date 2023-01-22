8bf3ec19cdb940139e920f674065496887ca6925
SHEKHAR SINGH <107771203+SHEKHARSINGH745681@users.noreply.github.com>
2023-01-22 10:23:29 +0530

    Add files via upload

---
+++ helloworld.java
@@ -0,0 +1,11 @@
+package java_dsa;
+
+public class helloworld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method 
+		System.out.println("helloworld");
+
+	}
+
+}
---
+++ sum.java
@@ -0,0 +1,15 @@
+package java_dsa;
+
+public class sum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a= 10;
+		int b=5;
+		int ans =(a*b)/(a-b);
+		System.out.println(ans);
+		
+
+	}
+
+}
