4187d348cab3cd46bab564bc6c9e1909cb1d2992
SHEKHAR SINGH <107771203+SHEKHARSINGH745681@users.noreply.github.com>
2023-01-23 09:04:49 +0530

    Add files via upload

---
+++ sum_2.java
@@ -0,0 +1,18 @@
+package java_dsa;
+
+import java.util.*;
+
+public class sum_2 extends sum {
+
+	public static void main(String[] args) {
+		//user se input lege,system.in isliye use 
+		//krte hai jab user value dega
+		Scanner sc=new Scanner(System.in);
+		String name =sc.next();
+		//nextLine full name print krana hai toh
+		//input store krane ke liye variable string define krge
+		System.out.println(name);
+
+	}
+
+}
---
+++ user_input_sum.java
@@ -0,0 +1,22 @@
+package java_dsa;
+
+import java.util.*;
+
+public class user_input_sum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		//nextint isliye use kiya hai kyuki integer value lege
+		int sum= a+b;
+		System.out.println(sum);
+		//"" ko use krte print main value wahi print ho jayegi
+		//jo"" ke andar hai
+		
+		
+
+	}
+
+}
