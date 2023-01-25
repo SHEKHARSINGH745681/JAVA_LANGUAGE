85cae603b235e95adf2f26cede202d45c0f2e5f8
SHEKHAR SINGH <107771203+SHEKHARSINGH745681@users.noreply.github.com>
2023-01-25 20:32:49 +0530

    Add files via upload

---
+++ forloop.java
@@ -0,0 +1,17 @@
+package java_dsa;
+
+public class forloop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//counter++ ka mtlb same hai counter=counter+1
+		for(int counter = 0;  counter < 11;  counter++) {
+			System.out.println(counter+" ");
+			//jo value "  " ke and hogi wahi value print hogi bas
+			// agar line pr print krna hota hai to
+			//counter+" "krna hota hai
+		}
+
+	}
+
+}
---
+++ loop_1.java
@@ -0,0 +1,13 @@
+package java_dsa;
+
+public class loop_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello wolrd");
+		System.out.println("hello wolrd");
+		System.out.println("hello wolrd");
+
+	}
+
+}
