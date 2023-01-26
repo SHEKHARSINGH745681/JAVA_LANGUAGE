de028ef18d97fa4225a99274d9f4fa45e80f7971
SHEKHAR SINGH <107771203+SHEKHARSINGH745681@users.noreply.github.com>
2023-01-27 00:34:25 +0530

    Add files via upload

---
+++ dowhile.java
@@ -0,0 +1,17 @@
+package java_dsa;
+
+public class dowhile {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i=0;
+		do {
+			System.out.println(i);
+			i++;
+		}while(i<11);
+			
+		}
+
+	}
+
+
