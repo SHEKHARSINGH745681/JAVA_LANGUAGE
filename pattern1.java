bbb6a19841e697cbce505a6a640e451258f3c9ff
SHEKHAR SINGH <107771203+SHEKHARSINGH745681@users.noreply.github.com>
2023-01-27 00:33:53 +0530

    Add files via upload

---
+++ pattern1.java
@@ -0,0 +1,29 @@
+package java_dsa;
+
+import java.util.Scanner;
+
+public class pattern1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		int n =sc.nextInt();
+		//outer loop
+		for(int i=1;i<=4;i++) {
+			//ineer loop
+		
+		for(int j=1;j<=4;j++) {
+			System.out.print("*");
+			//yaha pr print use krege bzc * ko har line 
+			//print nhii krna hai pr same line main star print krna hai
+		}
+		System.out.println();
+		// next line ke liye simply sysout println only
+		
+		
+				
+		}
+	}
+}
+
+
