8e320d0e7d5559f0dc6d72053d3876f69a8f8393
SHEKHAR SINGH <107771203+SHEKHARSINGH745681@users.noreply.github.com>
2023-01-26 23:45:30 +0530

    Add files via upload

---
+++ print_tableby_user.java
@@ -0,0 +1,20 @@
+package java_dsa;
+
+import java.util.Scanner;
+
+public class print_tableby_user {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		for(int i=1;i<11;i++) {
+			System.out.println(i*n);
+			
+		}
+		
+		
+
+	}
+
+}
