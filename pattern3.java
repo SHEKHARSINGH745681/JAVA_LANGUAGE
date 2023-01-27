c5221484927bf9c35210acd47bd47835b5669a05
SHEKHAR SINGH <107771203+SHEKHARSINGH745681@users.noreply.github.com>
2023-01-27 14:14:53 +0530

    Add files via upload

---
+++ pattern3.java
@@ -0,0 +1,21 @@
+package java_dsa;
+
+public class pattern3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n=4;
+		//outer loop
+		for(int i=n;i>=1;i--) {
+			//ineer loop
+			for(int j=1;j<=i;j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+			
+		}
+		
+
+	}
+
+}
