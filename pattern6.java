392e0592bcef7aa50ca10f759e008893e233c8c7
SHEKHAR SINGH <107771203+SHEKHARSINGH745681@users.noreply.github.com>
2023-01-28 23:46:29 +0530

    Add files via upload

---
+++ pattern6.java
@@ -0,0 +1,17 @@
+package java_dsa;
+
+public class pattern6 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n =5;
+		for(int i=1;i<=n;i++) {
+			
+			for(int j=1;j<=i;j++)
+				System.out.print(j+" ");
+		}
+		System.out.println();
+
+	}
+
+}
