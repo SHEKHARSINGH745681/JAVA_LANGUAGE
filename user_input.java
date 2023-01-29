86007312451f9790aa8592a2b55434e8f9efe1ba
SHEKHAR SINGH <107771203+SHEKHARSINGH745681@users.noreply.github.com>
2023-01-29 22:49:47 +0530

    Add files via upload

---
+++ user_input.java
@@ -0,0 +1,15 @@
+package lec6;
+
+public class user_input {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new -(System.in);
+		int n=sc.newInt();
+		int []arr=new int[n];
+		int[] ar=new int[n];
+		
+
+	}
+
+}
