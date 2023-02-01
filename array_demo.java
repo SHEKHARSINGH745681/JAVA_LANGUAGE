7f833f93d18f4550f666ac1ec5239311a599832c
SHEKHAR SINGH <107771203+SHEKHARSINGH745681@users.noreply.github.com>
2023-02-01 22:47:51 +0530

    Add files via upload

---
+++ array_demo.java
@@ -0,0 +1,15 @@
+package lec6;
+
+public class array_demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a;
+		int[] arr =new int [5];
+		System.out.println(arr);
+		arr[0]=10;
+		System.out.println(arr[0]);
+
+	}
+
+}
