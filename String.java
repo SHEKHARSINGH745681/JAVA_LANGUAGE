a87427e130d8f30329344584e87a04509b946b77
SHEKHAR SINGH <107771203+SHEKHARSINGH745681@users.noreply.github.com>
2023-02-02 15:52:35 +0530

    Add files via upload

---
+++ String.java
@@ -0,0 +1,24 @@
+package java_dsa;
+
+import java.util.Scanner;
+
+public class String {
+
+	public static void main(java.lang.String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		java.lang.String name=sc.nextLine();
+		System.out.println("my name is : "+ name);
+		
+		
+		
+		// string declaration
+		//String name ="shekhar";
+	//	String fullname="shekhar singh";
+		
+		
+		
+
+	}
+
+}
